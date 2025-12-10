48aa3bf77c4f1023bf9ac8ac93eeda11975a0dbb
krishnakumar A D <krishnakumarad7@gmail.com>
2025-12-10 16:25:19 +0530

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,7 +5,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:8080', 'http://127.0.0.1:8080'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:8080', 'http://127.0.0.1:8080','budget-tracker-krishnakumar.vercel.app
+','budget-tracker-l710q4wx8-krishs-projects-274a4f96.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
